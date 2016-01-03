768086ed762af4a15546ccaf99c757e7638204e3
piwrks <forschner65@gmail.com>
2016-01-03 16:32:05 +0100

    Update ESP8266-SHT21-HTU21-BMP180-DHT22-Thingspeak-logger.ino

--- ESP8266-SHT21-HTU21-BMP180-DHT22-Thingspeak-logger.ino
+++ ESP8266-SHT21-HTU21-BMP180-DHT22-Thingspeak-logger.ino
@@ -2,7 +2,26 @@
  *  This sketch sends data via HTTP GET requests to thingspeak service every 10 minutes
  *  You have to set your wifi credentials and your thingspeak key.
  *  Arduino 1.65
- *  thanks to Dani Eichhorn -> blog.squix.com
+ *  thanks to Dani Eichhorn -> http://blog.squix.ch
+ 
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
  */
 
 #include <Adafruit_BMP085.h>  //https://github.com/adafruit/Adafruit-BMP085-Library V1.0
